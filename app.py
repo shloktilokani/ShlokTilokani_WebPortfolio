9a0f24c13ff3259aa6e56c95b6d6cc081fa0851e
shloktilokani <tilokanishlok.work@gmail.com>
2024-11-07 21:26:44 +0530

    Updated Files

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import glob
 app = Flask(__name__)
 
 
-def load_json_files(folder_path):
+def load_experience_data(folder_path):
     """Load JSON files from a folder and return their data as a list."""
     data_list = []
     for file_path in glob.glob(os.path.join(folder_path, "*.json")):
@@ -40,6 +40,24 @@ def load_project_data(projects_folder):
     return project_data, sorted(unique_categories)
 
 
+def load_certificate_data(certificates_folder):
+    """Load project data from folders and include image paths."""
+    certificate_data = []
+
+    for certificate_name in os.listdir(certificates_folder):
+        certificate_path = os.path.join(certificates_folder, certificate_name)
+        if os.path.isdir(certificate_path):
+            json_path = os.path.join(certificate_path, "data.json")
+            image_path = find_image_file(certificate_path)
+
+            if os.path.exists(json_path):
+                with open(json_path, "r") as file:
+                    data = json.load(file)
+                    data["image"] = image_path or "/static/images/default.jpg"
+                    certificate_data.append(data)
+    return certificate_data
+
+
 def find_image_file(folder_path):
     """Find an image file with common extensions in a folder."""
     for ext in ["jpg", "png", "jpeg", "gif"]:
@@ -53,12 +71,13 @@ def find_image_file(folder_path):
 def home():
     experience_folder = os.path.join("static", "json", "experience")
     projects_folder = os.path.join("static", "json", "projects")
+    certificates_folder = os.path.join("static", "json", "certificates")
+    experience_content = load_experience_data(experience_folder)
+    certificate_content = load_certificate_data(certificates_folder)
 
-    # Load experience data
-    experience_content = load_json_files(experience_folder)
+    project_content, unique_categories = [], []
 
     # Check if the projects folder exists before trying to read it
-    project_content, unique_categories = [], []
     if os.path.exists(projects_folder):
         project_content, unique_categories = load_project_data(projects_folder)
     else:
@@ -69,6 +88,7 @@ def home():
         experience_content=experience_content,
         project_content=project_content,
         categories=unique_categories,
+        certificate_content=certificate_content,
     )
 
 
