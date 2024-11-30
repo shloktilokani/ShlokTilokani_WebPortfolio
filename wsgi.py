2c6a6cb664bf51d796eda81956af3bcd367b52fe
shloktilokani <tilokanishlok.work@gmail.com>
2024-12-01 00:43:41 +0530

    updated wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,1 @@
-import sys
-
-sys.path.insert(0, "/home2/shloktilokani/repositories/ShlokTilokaniWebPortfolio")
-
-from app import app as application  # Replace 'app' with your Flask app file/module
+from app import app as application
