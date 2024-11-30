4efed73c41f6c4e7f146c1e6eea0dc6cf43dd260
shloktilokani <tilokanishlok.work@gmail.com>
2024-12-01 00:24:43 +0530

    updated wsgi passenger file

---
+++ wsgi.py
@@ -0,0 +1,5 @@
+import sys
+
+sys.path.insert(0, "/home2/shloktilokani/repositories/ShlokTilokaniWebPortfolio")
+
+from app import app as application  # Replace 'app' with your Flask app file/module
