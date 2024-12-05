eb5be972cf4d4f4281ddf907120729a72cc818c8
shloktilokani <tilokanishlok.work@gmail.com>
2024-12-05 12:11:09 +0530

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from app import app as application
+import sys
+import os
 
-if __name__ == "__main__":
-    application.run()
+# Add the app directory to the Python path
+sys.path.insert(0, '/home2/shloktilokani/public_html/ShlokWebPortfolio')
+
+# Set the Flask app
+from app import app as application
